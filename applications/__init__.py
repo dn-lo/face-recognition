e835387991a91592b1d218c2d813cca588ad7541
Daniele Bruno <d-bruno@outlook.it>
2025-09-07 19:22:52 +0200

    Add init files for applications and tests (#5)

---
+++ applications/__init__.py
@@ -0,0 +1,9 @@
+"""Applications of the trained models.
+
+Rules:
+- This folder should not contain any business logic.
+- Models, utility functions, dataloaders and other reusable components should be placed in other
+folders and imported from the respective packages.
+- This folder can only contain scripts that demonstrate how to use the trained models for inference
+or other tasks.
+"""
