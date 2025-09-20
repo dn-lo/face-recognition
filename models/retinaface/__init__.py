d5ead95661cf3043b08a9b11aae1e07a4700c225
dn-lo <d-bruno@outlook.it>
2025-09-20 22:22:42 +0000

    Fix Ruff warnings

--- models/retinaface/__init__.py
+++ models/retinaface/__init__.py
@@ -1,3 +1,5 @@
-# RetinaFace pre-trained model
-# Original paper: Deng et al., 2019, arXiv:1905.00641
-# Original PyTorch implementation: https://github.com/biubug6/Pytorch_Retinaface
+"""RetinaFace pre-trained model.
+
+Original paper: Deng et al., 2019, arXiv:1905.00641
+Original PyTorch implementation: https://github.com/biubug6/Pytorch_Retinaface.
+"""
