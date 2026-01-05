e04f8410ef4cfee3d3f08b9ecdd1731c35090b78
Daniele Bruno <d-bruno@outlook.it>
2026-01-05 15:54:38 +0100

    Improve docs (#10)

--- models/retinaface/__init__.py
+++ models/retinaface/__init__.py
@@ -1,5 +1,6 @@
-"""RetinaFace pre-trained model.
+"""RetinaFace pre-trained face detection model.
 
 Original paper: Deng et al., 2019: https://arxiv.org/pdf/1905.00641
-Original PyTorch implementation: https://github.com/biubug6/Pytorch_Retinaface
+MIT-licensed PyTorch implementation: https://github.com/biubug6/Pytorch_Retinaface
+MIT-licensed weights: https://drive.google.com/drive/folders/1oZRSG0ZegbVkVwUd8wUIQx8W7yfZ_ki1
 """
