18dd3952fc347c014b3c802696d2b0659683ed22
dn-lo <d-bruno@outlook.it>
2026-02-15 16:18:32 +0000

    Init arcface

---
+++ models/arcface/__init__.py
@@ -0,0 +1,6 @@
+"""ArcFace pre-trained face recognition model.
+
+Original paper: Deng et al., 2018: https://arxiv.org/pdf/1801.07698
+Original, MIT-licensed PyTorch implementation: https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch
+Weights for non-commercial research use:
+"""
